4d4d4b6615f85f3ce812ddf0829f6fb146f2dcaf
fran-valls <fjvalls@gmail.com>
2023-06-30 14:29:59 +0200

    commit 30/6-2

--- src/app/components/suma/suma.component.ts
+++ src/app/components/suma/suma.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-suma',
@@ -6,9 +6,14 @@ import {Component, ElementRef} from '@angular/core';
   styleUrls: ['./suma.component.css']
 })
 export class SumaComponent {
-  public inputNum1! : ElementRef<HTMLInputElement>;
-  public inputNum2! : ElementRef<HTMLInputElement>;
-  public inputResultado! : ElementRef<HTMLInputElement>;
-  public botonSumar! : ElementRef<HTMLButtonElement>;
+  @ViewChild("num1") public inputNum1! : ElementRef<HTMLInputElement>;
+  @ViewChild("num2")public inputNum2! : ElementRef<HTMLInputElement>;
+  @ViewChild("resultado")public inputResultado! : ElementRef<HTMLInputElement>;
+  @ViewChild("btnSumar")public botonSumar! : ElementRef<HTMLButtonElement>;
+
+  public sumar(){
+    let resultado: number = this.inputNum1.nativeElement.valueAsNumber + this.inputNum2.nativeElement.valueAsNumber;
+    this.inputResultado.nativeElement.value = resultado + "";
+  }
 
 }
